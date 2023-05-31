f0b03c7e39f6f9c06bf2052b9cb8c4e2e5df6e2e
Luu Vu <vu7178@gmail.com>
2023-06-01 01:43:35 +0700

    SASS+ Router

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,11 @@
+function Upload() {
+    return (
+        <div className="content">
+            <div className="container">
+                <div className="content">Upload</div>
+            </div>
+        </div>
+    );
+}
+
+export default Upload;
